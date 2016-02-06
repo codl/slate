8b3c7375f5e87153b288969280ffc53d6947503b
codl (Corentin Delcourt) <codl@codl.fr>
2016-02-06 12:58:14 +0100

    picarto: avoid race condition if the chat is slow to load

--- chat_js/picarto.js
+++ chat_js/picarto.js
@@ -1,7 +1,13 @@
-console.log("hey");
-window.setTimeout(function(){
-    document.querySelector("#mainContainer").style.top = 0;
-    document.querySelector(".tse-scroll-content").style.height = "100%";
-    simpleMode = true;
-    simpleChat(true, true);
-}, 1000);
+(function(){
+    function prepare_chat(){
+        document.querySelector("#mainContainer").style.top = 0;
+        document.querySelector(".tse-scroll-content").style.height = "100%";
+        simpleMode = true;
+        simpleChat(true, true);
+    }
+
+    socket.on('connect', prepare_chat);
+    if(socket.connected){
+        prepare_chat();
+    }
+})();
